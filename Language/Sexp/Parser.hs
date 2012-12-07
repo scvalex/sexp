7cc6d82d1f6108c5df5d1814da74cb65ad134804
Alexandru Scvortov <scvalex@gmail.com>
2012-12-07 18:16:54 +0000

    throw parse exception when appropriate

--- Language/Sexp/Parser.hs
+++ Language/Sexp/Parser.hs
@@ -1,16 +1,29 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Language.Sexp.Parser (
-        Sexp(..), parse, sexpParser
+        Sexp(..), sexpParser,
+        ParseException(..), parse, parseExn
     ) where
 
 import Control.Applicative ( (<$>), (<*), (*>), many )
+import Control.Exception ( Exception )
+import qualified Control.Exception as CE
 import Data.Attoparsec.ByteString.Lazy ( Parser, Result(..) )
 import Data.Attoparsec.ByteString.Char8 ( char, space, takeWhile1, notInClass, (<?>) )
 import Data.Attoparsec.Combinator ( choice )
 import qualified Data.Attoparsec.ByteString.Lazy as A
 import Data.ByteString.Lazy as BS
+import Data.Typeable ( Typeable )
 
+-- | A 'ByteString'-based S-Expression.  You can a lazy 'ByteString'
+-- with 'parse'.
 data Sexp = List [Sexp] | Atom ByteString
           deriving ( Show )
+
+data ParseException = ParseException String ByteString
+                    deriving ( Show, Typeable )
+
+instance Exception ParseException
 
 -- | Parse S-Expressions from a lazy 'ByteString'.  If the parse was
 -- successful, @Right sexps@ is returned; otherwise, @Left (errorMsg,
@@ -18,11 +31,20 @@ data Sexp = List [Sexp] | Atom ByteString
 parse :: ByteString -> Either (String, ByteString) [Sexp]
 parse = resultToEither . A.parse (many sexpParser)
   where
-    resultToEither (Fail leftover _ctxs msg) = Left (msg, leftover)
+    resultToEither (Fail leftover _ctxs reason) =
+        Left (reason, leftover)
     resultToEither (Done leftover sexps) =
         if BS.null leftover
         then Right sexps
         else Left ("garbage at end", leftover)
+
+-- | A variant of 'parse' that throws a 'ParseException' if the parse
+-- fails.
+parseExn :: ByteString -> [Sexp]
+parseExn text =
+    case parse text of
+        Left (reason, leftover) -> CE.throw (ParseException reason leftover)
+        Right sexps             -> sexps
 
 -- | A parser for S-Expressions.  Ignoring whitespace, we follow the
 -- following EBNF:
