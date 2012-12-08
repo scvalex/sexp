32dba12ec066d992d4230da0e25a0b99f91048c9
Alexandru Scvortov <scvalex@gmail.com>
2012-12-08 16:05:13 +0000

    add preliminary support for generics-based reification

--- Data/Sexp.hs
+++ Data/Sexp.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Safe, OverloadedStrings #-}
+{-# LANGUAGE Safe, OverloadedStrings, DefaultSignatures, FlexibleContexts, TypeOperators #-}
 
 module Data.Sexp (
         Sexp(..), Sexpable(..),
@@ -6,6 +6,7 @@ module Data.Sexp (
     ) where
 
 import Data.ByteString.Lazy.Char8 as BS
+import GHC.Generics
 
 -- | A 'ByteString'-based S-Expression.  You can a lazy 'ByteString'
 -- with 'parse'.
@@ -15,6 +16,28 @@ data Sexp = List [Sexp] | Atom ByteString
 class Sexpable a where
     toSexp :: a -> Sexp
     fromSexp :: (Monad m) => Sexp -> m a
+
+    default toSexp :: (Generic a, GSexpable (Rep a)) => a -> Sexp
+    toSexp x = gToSexp (from x)
+
+class GSexpable a where
+    gToSexp :: a p -> Sexp
+
+instance GSexpable U1 where
+    gToSexp _ = List []
+
+instance (GSexpable a, GSexpable b) => GSexpable (a :*: b) where
+    gToSexp (x :*: y) = List [gToSexp x, gToSexp y]
+
+instance (GSexpable a, GSexpable b) => GSexpable (a :+: b) where
+    gToSexp (L1 x) = List [Atom "left", gToSexp x]
+    gToSexp (R1 x) = List [Atom "right", gToSexp x]
+
+instance (GSexpable a) => GSexpable (M1 i c a) where
+    gToSexp (M1 x) = gToSexp x
+
+instance (Sexpable a) => GSexpable (K1 i a) where
+    gToSexp (K1 x) = toSexp x
 
 instance Sexpable Int where
     toSexp n = Atom (pack (show n))
