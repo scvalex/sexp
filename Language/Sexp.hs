11a61eef0f44a38246ad7d62913a1c8adde9a016
Alexandru Scvortov <scvalex@gmail.com>
2013-01-21 21:14:11 +0000

    add simple pretty-printers

--- Language/Sexp.hs
+++ Language/Sexp.hs
@@ -1,6 +1,8 @@
 module Language.Sexp (
-        module Language.Sexp.Parser
+        module Language.Sexp.Parser,
+        module Language.Sexp.Printer
     ) where
 
 import Language.Sexp.Parser
 
+import Language.Sexp.Printer
---
+++ Language/Sexp/Printer.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Language.Sexp.Printer (
+        printHum, printMach
+    ) where
+
+import Data.ByteString.Lazy.Char8 as BS hiding ( map )
+import Data.Sexp ( Sexp(..), escape )
+
+-- | Pretty print a 'Sexp' with minimal formatting.  Suitable for
+-- machine processing.
+printMach :: Sexp -> ByteString
+printMach (Atom s) = escape s
+printMach (List xs) = BS.concat ["(", BS.intercalate " " (map printMach xs), ")"]
+
+-- | Pretty print a 'Sexp' in a human-friendly way.
+printHum :: Sexp -> ByteString
+printHum = printMach
