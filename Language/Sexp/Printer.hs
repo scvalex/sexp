d040cef8a8a9c55eb58ab5b06fecaccdb837574a
Alexandru Scvortov <scvalex@gmail.com>
2013-01-22 20:45:00 +0000

    don't quote strings thta don't have spaces in them

--- Language/Sexp/Printer.hs
+++ Language/Sexp/Printer.hs
@@ -4,13 +4,17 @@ module Language.Sexp.Printer (
         printHum, printMach
     ) where
 
+import Data.Attoparsec.ByteString.Char8 ( isSpace )
 import Data.ByteString.Lazy.Char8 as BS hiding ( map )
 import Data.Sexp ( Sexp(..), escape )
 
 -- | Pretty print a 'Sexp' with minimal formatting.  Suitable for
 -- machine processing.
 printMach :: Sexp -> ByteString
-printMach (Atom s)  = BS.concat ["\"", escape s, "\""]
+printMach (Atom s)  = let es = escape s
+                      in if BS.find isSpace es /= Nothing
+                         then BS.snoc (BS.cons '\"' es) '\"'
+                         else es
 printMach (List xs) = BS.concat ["(", BS.intercalate " " (map printMach xs), ")"]
 
 -- | Pretty print a 'Sexp' in a human-friendly way.
