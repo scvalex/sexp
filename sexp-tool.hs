8911309d06f5785704d01b948917f94760d32986
Alexandru Scvortov <scvalex@gmail.com>
2013-01-23 02:47:05 +0000

    two phase read/write for testing purposes

--- sexp-tool.hs
+++ sexp-tool.hs
@@ -2,20 +2,21 @@
 
 module Main where
 
-import Data.ByteString.Lazy.Char8 as BS
+import Data.ByteString.Lazy.Char8 as BS hiding ( head )
 import Data.Version ( showVersion )
 import Language.Sexp ( parseExn, printHum )
 import Paths_sexp ( version )
 import System.Console.CmdArgs
 import Text.Printf ( printf )
 
-data Modes = Print { files :: [FilePath] }
+data Modes = Print { files :: [FilePath], twoPass :: Bool }
            deriving ( Show, Data, Typeable )
 
 sexpModes :: [Modes]
 sexpModes =
-    [ Print { files = def &= typ "FILE*" &= args }
-             &= help "pretty-print S-expressions from files or stdin"
+    [ Print { files = def &= typFile &= args
+            , twoPass = def &= typ "BOOL"
+            } &= help "pretty-print S-expressions from files or stdin"
     ]
     &= program "sexp"
     &= summary (printf "sexp v%s - S-Expression magic" (showVersion version))
@@ -23,8 +24,15 @@ sexpModes =
 readPrintSexp :: FilePath -> IO ()
 readPrintSexp fp = mapM_ (BS.putStrLn . printHum) . parseExn =<< BS.readFile fp
 
+readPrintReadPrintSexp :: FilePath -> IO ()
+readPrintReadPrintSexp fp =
+    mapM_ (BS.putStrLn . printHum . head . parseExn . printHum) . parseExn =<< BS.readFile fp
+
 main :: IO ()
 main = do
     opts <- cmdArgs $ modes sexpModes
     case opts of
-        Print fs -> mapM_ readPrintSexp fs
+        Print fs tp -> do
+            if tp
+                then mapM_ readPrintReadPrintSexp fs
+                else mapM_ readPrintSexp fs
