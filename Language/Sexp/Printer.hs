ff7455d922ba1e586a0d94502ef1689835ac20b2
Alexandru Scvortov <scvalex@gmail.com>
2013-01-23 15:02:04 +0000

    when printing, quote everything that's not and identifier

--- Language/Sexp/Printer.hs
+++ Language/Sexp/Printer.hs
@@ -4,17 +4,20 @@ module Language.Sexp.Printer (
         printHum, printMach
     ) where
 
-import Data.Attoparsec.ByteString.Char8 ( isSpace )
-import Data.ByteString.Lazy.Char8 as BS hiding ( map )
+import Data.ByteString.Lazy.Char8 as BS hiding ( map, elem )
 import Data.Sexp ( Sexp(..), escape )
 
 -- | Pretty print a 'Sexp' with minimal formatting.  Suitable for
 -- machine processing.
 printMach :: Sexp -> ByteString
 printMach (Atom s)  = let es = escape s
-                      in if BS.find isSpace es /= Nothing || '\\' `BS.elem` es
+                      in if shouldQuote es
                          then BS.snoc (BS.cons '\"' es) '\"'
                          else es
+  where
+    shouldQuote es = BS.find (\c -> (c < 'A' || 'z' < c)
+                                    && (c < '0' || '9' < c)
+                                    && not (c `elem` "-_")) es /= Nothing
 printMach (List xs) = BS.concat ["(", BS.intercalate " " (map printMach xs), ")"]
 
 -- | Pretty print a 'Sexp' in a human-friendly way.
